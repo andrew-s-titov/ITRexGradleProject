cbd563029b0d72f29309dd12613a13eb9f8fcb97
andrew.s.titov <andrew.s.titov@gmail.com>
2021-10-10 17:36:19 +0300

    Commit gradle with profiles

---
+++ README.md
@@ -0,0 +1,5 @@
+**To specify the profile, run commands with 'env' property, e.g.:**  
+*gradlew build -Penv=stable*  
+*gradlew clean -Penv=dev*
+
+P.s. "dev" profile is used by default, when the 'env' property isn't specified.
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void createGreetingTest() {
+        Assertions.assertEquals("Hello, Peter!", App.createGreeting("Peter"));
+    }
+}
